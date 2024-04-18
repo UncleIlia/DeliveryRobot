c1fe774ccb27d340af4356f0ff8ee62ec60148ea
IliaNovikov <ilja.nowikow2013@yandex.ru>
2024-04-18 17:20:09 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -6,32 +6,27 @@ public class Main {
     public static Map<Integer, Long> sizeToFreq = new HashMap<>();
 
     public static void main(String[] args) {
-        List<String> listOfRandomStrings = new ArrayList<>();
         List<Integer> listForCountR = new ArrayList<>();
 
-        new Thread(() -> {
-            synchronized (listOfRandomStrings) {
-                for (int i = 0; i < 1000; i++) {
-                    listOfRandomStrings.add(generateRoute("RLRFR", 100));
-                    int count = listOfRandomStrings.get(i).length() - listOfRandomStrings.get(i).replace(String.valueOf('R'), "").length();
+        for (int i = 0; i < 1000; i++) {
+            new Thread(() -> {
+                synchronized (listForCountR) {
+                    String randomString = generateRoute("RLRFR", 100);
+                    int count = randomString.length() - randomString.replace(String.valueOf('R'), "").length();
                     listForCountR.add(count);
+                    sizeToFreq = listForCountR.stream()
+                            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
                 }
-            }
-        }).start();
+            }).start();
+        }
 
-        new Thread(() -> {
-            synchronized (listOfRandomStrings) {
-                sizeToFreq = listForCountR.stream()
-                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-                Map<Integer, Long> sortedMap = sortMapByValueDescending(sizeToFreq);
-                List<Integer> listOfKeys = new ArrayList<>(sortedMap.keySet());
-                System.out.println("Самое частое количество повторений " + listOfKeys.get(0) + " (встретилось " + sortedMap.get(listOfKeys.get(0)) + " раз)");
-                System.out.println("Другие размеры:");
-                for (int i = 1; i < listOfKeys.size(); i++) {
-                    System.out.println("- " + listOfKeys.get(i) + " (" + sortedMap.get(listOfKeys.get(i)) + " раз)");
-                }
-            }
-        }).start();
+        Map<Integer, Long> sortedMap = sortMapByValueDescending(sizeToFreq);
+        List<Integer> listOfKeys = new ArrayList<>(sortedMap.keySet());
+        System.out.println("Самое частое количество повторений " + listOfKeys.get(0) + " (встретилось " + sortedMap.get(listOfKeys.get(0)) + " раз)");
+        System.out.println("Другие размеры:");
+        for (int i = 1; i < listOfKeys.size(); i++) {
+            System.out.println("- " + listOfKeys.get(i) + " (" + sortedMap.get(listOfKeys.get(i)) + " раз)");
+        }
     }
 
     public static String generateRoute(String letters, int length) {
